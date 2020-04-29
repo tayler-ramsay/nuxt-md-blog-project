e4c004cac4b7276ae412a2742e83c24e48cfd49f
Tayler Ramsay <tramsay@versatilets.com>
2020-04-29 13:22:22 -0400

    this is it

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between">
       <h1><nuxt-link to="/">Versatile Blog</nuxt-link></h1>
       <div class="flex">
-        <div class="mr-6"><nuxt-link to="/about">About</nuxt-link></div>
+        <div class="mr-6 text-xl"><nuxt-link to="/about">About</nuxt-link></div>
         <div><nuxt-link to="/team">Team</nuxt-link></div>
       </div>
     </div>
