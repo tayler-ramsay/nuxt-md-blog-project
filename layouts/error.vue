38857ba40769ed5a048f62fad9f9898c7cbc0ed5
tramsay <tramsay@versatilets.com>
2020-05-22 12:43:28 -0400

    add 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">
-      Page not found
-    </h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+  <div
+    class="bg-slate wrapper border-8 border-black flex flex-col lg:pl-20 p-3 pt-40"
+  >
+    <div v-if="error.statusCode === 404">
+      <h1 class="font-bold text-green mb-5 text-7xl heading">Opps!</h1>
+      <p
+        class="text-blue-light font-semibold text-2xl lg:text-3xl max-w-lg mb-5"
+      >
+        We can't seem to find the page you are looking for
+      </p>
+      <p class="font-semibold text-green mb-5">
+        Error code: {{ error.statusCode }}
+      </p>
+    </div>
+    <div v-else>
+      <h1>An error as occured {{ error.statusCode }}</h1>
+    </div>
+    <nuxt-link to="/" class="text-white flex"
+      ><BaseArrowL color="#fff" class="mr-3" />Home page</nuxt-link
+    >
   </div>
 </template>
 <script>
@@ -13,3 +27,16 @@ export default {
   layout: 'my-error-layout'
 }
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 70vh;
+}
+.heading {
+  font-size: 6rem;
+  line-height: 9rem;
+  @media (min-width: 768px) {
+    font-size: 8rem;
+  }
+}
+</style>
